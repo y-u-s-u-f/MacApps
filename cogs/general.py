1e7364ca570651d4c83935cabb751e4a95d17cb6
YufusKhan <67281394+YufusKhan@users.noreply.github.com>
2024-01-27 12:18:40 -0800

    Refactor ping command to include latency in response

--- cogs/general.py
+++ cogs/general.py
@@ -10,8 +10,8 @@ class General(commands.Cog):
 
     @app_commands.command(name='ping', description="Sends the bot's latency")
     async def ping(self, interaction: discord.Interaction):
-        embed = discord.Embed(title=f"Pong! {round(self.bot.latency * 1000)}ms")
-        await interaction.response.send_message(embed=embed, color=discord.Color.green())
+        embed = discord.Embed(title=f"Pong!", description=f"Latency: {round(self.bot.latency * 1000)}ms", color=discord.Color.green())
+        await interaction.response.send_message(embed=embed)   
 
     @commands.Cog.listener()
     async def on_ready(self):
