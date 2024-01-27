b9da95e2274c7f6942f90c355b5412cc22a7af6b
YufusKhan <67281394+YufusKhan@users.noreply.github.com>
2024-01-27 11:13:59 -0800

    Add General cog with ping command

---
+++ cogs/bot.py
@@ -0,0 +1,14 @@
+import discord
+import datetime
+from discord.ext import commands
+from discord import app_commands
+
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name='ping', description="Sends the bot's latency")
+    async def ping(self, interaction: discord.Interaction):
+        embed = discord.Embed(title=f"Pong! {round(self.bot.latency * 1000)}ms")
+        await interaction.response.send_message(embed=embed, color=discord.Color.green())
