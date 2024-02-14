19df380df1604a388eaa9671005cfaef4fa935b2
YufusKhan <67281394+YufusKhan@users.noreply.github.com>
2024-02-13 20:19:49 -0800

    Fixed a bug if the app description was over 1024 characters

--- cogs/partners.py
+++ cogs/partners.py
@@ -6,15 +6,14 @@ from discord import app_commands
 
 class PartnerModal(discord.ui.Modal, title='Partner with us!'):
     app_name = discord.ui.TextInput(label='App Name(s)', placeholder='Your app name(s)', style=discord.TextStyle.short, required=True)
-    app_desc = discord.ui.TextInput(label='Long App Description(s)', placeholder='Your app description(s) - 1 paragraph each.', style=discord.TextStyle.long, required=False)
+    app_desc = discord.ui.TextInput(label='Long App Description(s)', placeholder='Your app description(s) - 1 paragraph each.', style=discord.TextStyle.long, required=True)
     app_link = discord.ui.TextInput(label='App Link(s)', placeholder='Your app link(s)', required=True)
     notes = discord.ui.TextInput(label='Notes', placeholder='Any additional information you would like to provide', style=discord.TextStyle.long, required=False)
 
     async def on_submit(self, interaction: discord.Interaction):
-        embed=discord.Embed(title='New Partner Application!', description=f'{interaction.user.mention} has submitted an application!', timestamp=datetime.datetime.now(), color=discord.Color.green())
+        embed=discord.Embed(title='New Partner Application!', description=f'{interaction.user.mention} has submitted an application!\n\n### Description:\n\n'+self.app_desc.value, timestamp=datetime.datetime.now(), color=discord.Color.green())
         embed.set_footer(text=f'Submitted by {interaction.user.name}', icon_url=interaction.user.avatar.url)
         embed.add_field(name='App Name(s)', value=self.app_name.value, inline=False)
-        embed.add_field(name='App Description(s)', value=self.app_desc.value, inline=False) if self.app_desc.value else None
         embed.add_field(name='App Link(s)', value=self.app_link.value, inline=False)
         embed.add_field(name='Notes', value=self.notes.value, inline=False) if self.notes.value else None
         # Create a channel for that partner submission:
